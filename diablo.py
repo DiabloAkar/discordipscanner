15104cff6249ffc060decf44a727fb028944d3e2
DiabloAkar <86086068+DiabloAkar@users.noreply.github.com>
2021-10-12 19:54:33 +0300

    Add files via upload

---
+++ diablo.py
@@ -0,0 +1,99 @@
+import subprocess
+from pip._vendor.colorama import Fore
+import threading
+import time
+import os
+#import layn
+# Bu kodları değiştiren ahkalsızdır.
+
+subprocess.call('start https://discord.gg/DDcXf472BF', shell=True)
+subprocess.call('start https://github.com/DiabloAkar', shell=True)
+
+#diabloakar
+bammer = '''
+
+████████▄   ▄█     ▄████████ ▀█████████▄   ▄█        ▄██████▄  
+███   ▀███ ███    ███    ███   ███    ███ ███       ███    ███ 
+███    ███ ███▌   ███    ███   ███    ███ ███       ███    ███ 
+███    ███ ███▌   ███    ███  ▄███▄▄▄██▀  ███       ███    ███ 
+███    ███ ███▌ ▀███████████ ▀▀███▀▀▀██▄  ███       ███    ███ 
+███    ███ ███    ███    ███   ███    ██▄ ███       ███    ███ 
+███   ▄███ ███    ███    ███   ███    ███ ███▌    ▄ ███    ███ 
+████████▀  █▀     ███    █▀  ▄█████████▀  █████▄▄██  ▀██████▀  
+                                          ▀                    
+
+'''
+
+print()
+print(Fore.RED+bammer)
+print(Fore.WHITE+'|'+Fore.GREEN+'AkarGang | 1'+Fore.WHITE+'| > '+Fore.WHITE+'|'+Fore.GREEN+' Europe'+Fore.WHITE+'|')
+print(Fore.WHITE+'|'+Fore.GREEN+'AkarGang | 2'+Fore.WHITE+'| > '+Fore.WHITE+'|'+Fore.GREEN+' Russia'+Fore.WHITE+'|')
+print()
+sunucu1 = input(Fore.WHITE+'|'+Fore.GREEN+'Sunucu Türünü Seç'+Fore.WHITE+'| > ')
+
+if sunucu1 == '1':
+    print()
+    os.system('cls')
+    print(Fore.RED+bammer)
+    print(Fore.WHITE+'|'+Fore.GREEN+'AkarGang | Seçtiğiniz Ülke'+Fore.WHITE+'| > '+Fore.WHITE+'|'+Fore.GREEN+' Europe'+Fore.WHITE+'|')
+    taramaen = input(Fore.WHITE+'|'+Fore.GREEN+'Sayıyı girin'+Fore.WHITE+'| > ')
+    time.sleep(1)
+    os.system('cls')
+    print(Fore.RED+bammer)
+    print(Fore.RED+'AkarGang & TheWebDark Discord VDS Scanner')
+    time.sleep(1)
+    print(Fore.YELLOW+'Renk ayarlanıyor.')
+    time.sleep(0.5)
+    print(Fore.LIGHTMAGENTA_EX+'Renk ayarlanıyor..')
+    time.sleep(1)
+    print(Fore.BLUE+'Renk ayarlanıyor...')
+    time.sleep(1)
+    print(Fore.MAGENTA+'Renk ayarlandı!')
+    time.sleep(1)
+    os.system('cls')
+    print(Fore.BLUE+bammer)
+    time.sleep(1)
+    print(Fore.WHITE+'AkarGang & TheWebDark Discord VDS Scanner')
+    os.system('ping -t eu-central'+taramaen+'.discord.gg')
+
+if sunucu1 == '2':
+    print()
+    os.system('cls')
+    print(Fore.RED+bammer)
+    print(Fore.WHITE+'|'+Fore.GREEN+'AkarGang | Seçtiğiniz Ülke'+Fore.WHITE+'| > '+Fore.WHITE+'|'+Fore.GREEN+' Russia'+Fore.WHITE+'|')
+    taramaru = input(Fore.WHITE+'|'+Fore.GREEN+'Sayıyı girin'+Fore.WHITE+'| > ')
+    time.sleep(1)
+    print(Fore.RED+bammer)
+    print(Fore.RED+'AkarGang & TheWebDark Discord VDS Scanner')
+    time.sleep(1)
+    print(Fore.YELLOW+'Renk ayarlanıyor.')
+    time.sleep(1)
+    print(Fore.LIGHTMAGENTA_EX+'Renk ayarlanıyor..')
+    time.sleep(1)
+    print(Fore.BLUE+'Renk ayarlanıyor...')
+    time.sleep(1)
+    print(Fore.WHITE+'Renk ayarlandı!')
+    time.sleep(1)
+    print(Fore.BLUE+bammer)
+    time.sleep(1)
+    print(Fore.WHITE+'AkarGang & TheWebDark Discord VDS Scanner')
+    os.system('ping -t russia'+taramaru+'.discord.gg')
+
+else:
+    print('Yanlış tuşlama yaptınız program kendisini 5 saniye sonra kapatacaktır!')
+    time.sleep(1)
+    print('1')
+    time.sleep(1)
+    print('2')
+    time.sleep(1)
+    print('3')
+    time.sleep(1)
+    print('4')
+    time.sleep(1)
+    print('5')
+    time.sleep(1)
+    print(Fore.RED+'PROGRAM KENDİNİ KAPATIYOR')
+    time.sleep(3)
+
+    
+#diabloakar
